72c88e9da5a1da3a6733d6481ccce1fc71d4547f
NMyers00 <nmyers1@student.fitchburgstate.edu>
2023-04-25 11:41:58 -0400

    course layout

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 10f * Time.deltaTime, 0f, Space.Self);
     }
 }
